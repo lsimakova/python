6d87ebc9bf615e452f04bf5acc67a114044cdcf0
lsimakova <43305276+lsimakova@users.noreply.github.com>
2018-09-15 16:51:00 -0700

    Create lists_of_things.py

---
+++ lists_of_things.py
@@ -0,0 +1,14 @@
+names = ['Lora', 'Dima', 'Olga', 'Pavel']
+print(names[0])
+print(names[2])
+print(names)
+
+names.append('Nathan')
+print(names)
+
+names.pop(0)
+print(names)
+
+sorted_names = sorted(names)
+print(names)
+print(sorted_names)
