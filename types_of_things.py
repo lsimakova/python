9f7a2f1bd7258c48d63dea9a6b9ae595939c0c62
lsimakova <43305276+lsimakova@users.noreply.github.com>
2018-09-15 16:50:30 -0700

    Create types_of_things.py

---
+++ types_of_things.py
@@ -0,0 +1,12 @@
+a = 1                   # integer
+b = 2.0                 # floating number
+c = 'Lora Simakova'     # string
+d = a > 2               # boolean (True or False)
+
+print(a + 1)
+print(1 + a)
+print(b)
+print(b + 1)
+print(c + ' is Cool')
+print(d)
+print(not d)
