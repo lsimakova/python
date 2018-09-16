9e0cf8ce3ea49330057d4fbb8da802ccf3c5f47b
Lora Simakova <lora.p.simakova@gmail.com>
2018-09-16 13:43:41 -0700

    Added Nikki.

--- turtle/triangle.py
+++ turtle/triangle.py
@@ -3,14 +3,14 @@ import turtle
 nikki = turtle.Turtle()
 nikki.speed(30)
 nikki.pensize(4)
-nikki.setheading(45)
+nikki.setheading(0)
 
 nikki.pencolor('red')
 nikki.forward(100)
-nikki.left(60)
+nikki.left(120)
 nikki.pencolor('green')
 nikki.forward(100)
-nikki.left(60)
+nikki.left(120)
 nikki.pencolor('blue')
 nikki.forward(100)
 
