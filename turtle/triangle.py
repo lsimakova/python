a410892dbb83a6da0c45617f46c4ba6cafb648d8
Pavel Simakov <pavel@vokamis.com>
2018-09-16 12:42:34 -0700

    Update.

---
+++ turtle/triangle.py
@@ -0,0 +1,17 @@
+import turtle
+
+nikki = turtle.Turtle()
+nikki.speed(30)
+nikki.pensize(4)
+nikki.setheading(45)
+
+nikki.pencolor('red')
+nikki.forward(100)
+nikki.left(60)
+nikki.pencolor('green')
+nikki.forward(100)
+nikki.left(60)
+nikki.pencolor('blue')
+nikki.forward(100)
+
+turtle.done()
