5d0bfd77344c2c7dcdeeb70160efd542f4a41fe0
Lora Simakova <lora.p.simakova@gmail.com>
2018-09-22 10:20:33 -0700

    Added Square.

---
+++ turtle/square.py
@@ -0,0 +1,20 @@
+import turtle
+
+nikki = turtle.Turtle()
+nikki.speed(30)
+nikki.pensize(4)
+nikki.setheading(0)
+
+nikki.pencolor('red')
+nikki.forward(100)
+nikki.left(90)
+nikki.pencolor('green')
+nikki.forward(100)
+nikki.left(90)
+nikki.pencolor('blue')
+nikki.forward(100)
+nikki.pencolor('yellow')
+nikki.left(90)
+nikki.forward(100)
+
+turtle.done()
